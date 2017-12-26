ed14f72075976f58536712f9fca9f60820f6f9d5
onmax <maximogarciamtnez@gmail.com>
2017-12-26 11:50:12 +0100

    parte 1 terminada

--- practicaPPS/delReves/delReves.c
+++ practicaPPS/delReves/delReves.c
@@ -1,35 +1,32 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 int main (int argc,char **argv){
-
-	int c;
-  int nchar = 0;
-  FILE *fd;
-
-  for(int i = 1; i < argc; i++){
-    char str[2048];
-    char res[2048];
-  	fd = fopen(argv[i],"rt");
-  	if(fd == NULL){
-  		printf("Error al tratar de leer el archivo");
-  		return 1;
-  	}
-    int contador = 0;
-    while((c=fgetc(fd))!=EOF){
-      str[contador] = c;
-      contador ++;
-      nchar ++;
-    }
-    res[contador]= 0;
-    contador = 0;
-    while(nchar > 0){
-      res[nchar]=str[contador];
-      contador ++;
-      nchar --;
-    }
-    puts(res);
-  	fclose(fd);
-  }
+	if(strcmp(argv[1], "-h") == 0){
+		printf("delreves: Uso: delreves [ fichero... ]\n");
+		printf("delreves: Invierte el contenido de las lineas de los ficheros (o de la entrada).\n");
+	}else{
+		FILE *fp;
+		int c;
+		int i = 0;
+		int str[2048];
+		fp = fopen("prueba1.txt","r");
+		while(!feof(fp)) {
+		  c = fgetc(fp);
+			str[i]=c;
+			if(c == '\n'){
+				while(i != (-1)){
+					printf("%c",str[i]);
+					i --;
+				}
+				i = 0;
+			}else{
+				i ++;
+			}
+		}
+		fclose(fp);
+		return(0);
+	}
 	return 0;
 }
