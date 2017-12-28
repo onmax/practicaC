ce55e1d5884bbda4c2a3ad29993389a32e9f25eb
onmax <maximogarciamtnez@gmail.com>
2017-12-28 20:20:27 +0100

    Avanzando en ejercicio 1

--- practicaPPS/delReves/delReves.c
+++ practicaPPS/delReves/delReves.c
@@ -1,32 +1,48 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
+#define MAX 2048
 int main (int argc,char **argv){
 	if(strcmp(argv[1], "-h") == 0){
 		printf("delreves: Uso: delreves [ fichero... ]\n");
 		printf("delreves: Invierte el contenido de las lineas de los ficheros (o de la entrada).\n");
 	}else{
+	// 	FILE *fp;
+	// 	int c;
+	// 	int i = 0;
+	// 	int str[2048];
+	// 	fp = fopen("prueba1.txt","r");
+	// 	while(!feof(fp)) {
+	// 	  c = fgetc(fp);
+	// 		str[i]=c;
+	// 		if(c == '\n'){
+	// 			while(i != (-1)){
+	// 				printf("%c",str[i]);
+	// 				i --;
+		// 			}
+	// 			i = 0;
+	// 		}else{
+	// 			i ++;
+	// 		}
+	// 	}
+	// 	fclose(fp);
+	// 	return(0);
 		FILE *fp;
-		int c;
-		int i = 0;
-		int str[2048];
-		fp = fopen("prueba1.txt","r");
+		fp = fopen("prueba1.txt" , "r");
+		char *str;
+		char *str2;
+		char temp;
+		int longitud;
 		while(!feof(fp)) {
-		  c = fgetc(fp);
-			str[i]=c;
-			if(c == '\n'){
-				while(i != (-1)){
-					printf("%c",str[i]);
-					i --;
-				}
-				i = 0;
-			}else{
-				i ++;
+			fgets(str,MAX,fp);
+			longitud = strlen(str);
+			for(int i-1 = longitud; i>= 0; i--){
+				temp = str[longitud-i];
+				str2[i] = temp;
 			}
+			fputs(str2, stdout);
 		}
 		fclose(fp);
-		return(0);
 	}
 	return 0;
 }
--- practicaPPS/secuencia.c
+++ practicaPPS/secuencia.c
@@ -2,14 +2,13 @@
 #include<stdlib.h>
 
 void imprimir(double inicio,double fin,double salto){
-  printf("Salto %d\n", salto);
-  for(int i = inicio; i<=fin; i += salto){
-    printf("%d\n", i);
+  for(double i = inicio; i<=fin; i += salto){
+    printf("\t%g\n", i);
   }
 }
 int main (int argc,char **argv){
   if(argc == 1){imprimir(1,10,1);}
-  if(argc == 2){imprimir(1,atoi(argv[1]),1);}
-  if(argc == 3){imprimir(atoi(argv[2]),atoi(argv[1]),1);}
-  if(argc == 4){imprimir(atoi(argv[2]),atoi(argv[1]),atoi(argv[3]));}
+  if(argc == 2){imprimir(1,atof(argv[1]),1);}
+  if(argc == 3){imprimir(atof(argv[2]),atof(argv[1]),1);}
+  if(argc == 4){imprimir(atof(argv[2]),atof(argv[1]),atof(argv[3]));}
 }
