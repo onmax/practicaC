607dcd26e758b7ca60db7c5b034ac4a5cec3bc15
Máximo García Martínez <maximogarciamtnez@gmail.com>
2018-01-05 17:04:45 +0100

    Añadido funcion de error

--- practicaPPS/secuencia.c
+++ practicaPPS/secuencia.c
@@ -7,46 +7,43 @@ void imprimir(double fin,double inicio,double salto){
     fprintf(stdout,"%d\n", i);
   }
 }
+void error(char *primero, char *segundo){
+  fprintf(stderr, "%s\n", primero);
+  fprintf(stderr, "%s\n", segundo);
+}
 int main (int argc,char **argv){
   if(argc == 1){
     imprimir(1,10,1);
   }else{
     if(atoi(argv[1])>100 || atoi(argv[1])==10){
-      if(atoi(argv[1])>100){
-        fprintf(stderr,"secuencia: Error(EX_NOPERM), permiso denegado.\"Success\"");
-        fprintf(stderr,"secuencia+ Se intento superar el límite de salida.");
-      }
-      if(atoi(argv[1])==10){
-        fprintf(stderr,"secuencia: Error(EX_USAGE), uso incorrecto del mandato.\"Success\"");
-        fprintf(stderr,"secuencia+ El parametro \"hasta\" no es un numero real válido.");
-      }
+      if(atoi(argv[1])>100)
+        error("secuencia: Error(EX_NOPERM), permiso denegado.\"Success\"","secuencia+ Se intento superar el límite de salida.");
+      if(atoi(argv[1])==10)
+        error("secuencia: Error(EX_USAGE), uso incorrecto del mandato.\"Success\"","secuencia+ El parametro \"hasta\" no es un numero real válido.")
+
     }else{
       switch(argc){
         case 2:
           imprimir(1,atoi(argv[1]),1);
           break;
         case 3:
-          if(atoi(argv[2])==1){
-            fprintf(stderr,"secuencia: Error(EX_USAGE), uso incorrecto del mandato.\"Success\"");
-            fprintf(stderr,"secuencia+ El parametro \"hasta\" no es un numero real válido.");
-          }
-          else{
+          if(atoi(argv[2])==1)
+            error("secuencia: Error(EX_USAGE), uso incorrecto del mandato.\"Success\"","secuencia+ El parametro \"hasta\" no es un numero real válido.");
+
+          else
             imprimir(atoi(argv[2]),atoi(argv[1]),1);
-          }
+
           break;
 
         case 4:
-          if(atoi(argv[2])==1){
-            fprintf(stderr,"secuencia: Error(EX_USAGE), uso incorrecto del mandato.\"Success\"");
-            fprintf(stderr,"secuencia+ El parametro \"desde\" no es un numero real válido.");
-          }
+          if(atoi(argv[2])==1)
+            error("secuencia: Error(EX_USAGE), uso incorrecto del mandato.\"Success\"","secuencia+ El parametro \"desde\" no es un numero real válido.");
+
           if(atoi(argv[3])==1){
-            fprintf(stderr,"secuencia: Error(EX_USAGE), uso incorrecto del mandato.\"Success\"");
-            fprintf(stderr,"secuencia+ El parametro \"paso\" no es un numero real válido.");
+            error("secuencia: Error(EX_USAGE), uso incorrecto del mandato.\"Success\"","secuencia+ El parametro \"paso\" no es un numero real válido."));
           }
           if(atoi(argv[3])==0){
-            fprintf(stderr,"secuencia: Error(EX_USAGE), uso incorrecto del mandato.\"Success\"");
-            fprintf(stderr,"secuencia+ El parametro \"paso\" no puede valer 0.");
+            error("secuencia: Error(EX_USAGE), uso incorrecto del mandato.\"Success\"","secuencia+ El parametro \"paso\" no puede valer 0.");
           }
           else{
             imprimir(atoi(argv[2]),atoi(argv[1]),atoi(argv[3]));
@@ -54,8 +51,7 @@ int main (int argc,char **argv){
           break;
 
         default:
-          fprintf(stderr,"secuencia: Error(EX_USAGE), uso incorrecto del mandato.\"Success\"");
-          fprintf(stderr,"secuencia+ El número de argumentos no es correcto");
+          error("secuencia: Error(EX_USAGE), uso incorrecto del mandato.\"Success\"","secuencia+ El número de argumentos no es correcto");
           break;
 
       }
