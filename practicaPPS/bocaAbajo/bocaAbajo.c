fd5a9df4d8ad5aad99db63820d60f31618cdac5e
Máximo García Martínez <maximogarciamtnez@gmail.com>
2018-01-07 12:21:50 +0100

    Avanzando en bocaAbajo

--- practicaPPS/bocaAbajo/bocaAbajo.c
+++ practicaPPS/bocaAbajo/bocaAbajo.c
@@ -5,24 +5,41 @@
 
 int getN(FILE *fp){
 	int contador = 0;
-	while(!feof(fp)){contador++;}
+
+	while(!feof(fp)){
+		contador++;
+	}
+	printf("as\n");
 	return contador;
 }
 
-int main (int argc,char **argv){
+int main (int argc,char *argv[]){
 	if(strcmp(argv[1], "-h") == 0){
 		printf("bocabajo: Uso: bocabajo [ fichero... ]\n");
 		printf("bocabajo: Invierte el orden de las lineas de los ficheros (o de la entrada).\n");
 	}else{
 		int i;
 		for(i=argc-1;i>0;i--){
+			char *archivo =argv[1];
 			FILE *fp;
-			fp = fopen(argv[i] , "r");
-			int tam = getN(fp);
-			char string[tam];
-			// while(fgets(string, tam, fp) != NULL) {
-			// 	printf("%s\n", string);
-			// }
+			fp = fopen(archivo , "r");
+			// int tam = getN(fp);
+			// char string[tam];
+			if(fp == NULL) {
+		    printf("Error opening file");
+   		}else{
+				char *arr[5];
+				int j = 0;
+				char str[2048];
+				while(fgets(str,2048,fp) != NULL){
+					arr[j] = str;
+					j ++;
+				}
+				j --;
+				for(j;j>=0;j--){
+					printf("%s\n", arr[j]);
+				}
+			}
 			fclose(fp);
 		}
 	}
