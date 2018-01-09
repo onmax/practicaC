3f15f5df6c20b64a411e074ecea149f5ba1d7ce0
rcarbajsa <raulcarbajosa7@hotmail.com>
2018-01-09 17:41:19 +0100

    Acabado

--- practicaPPS/delReves.c
+++ practicaPPS/delReves.c
@@ -1,41 +1,75 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include"auxiliar.c"
+#include<sysexits.h>
 #define MAX 2048
-
+#include<errno.h>
 int main (int argc,char **argv){
-	if(strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0){
-			fprintf(stdout, "delreves: Uso: delreves [ fichero... ]\n");
-			fprintf(stdout, "delreves: Invierte el contenido de las lineas de los ficheros (o de la entrada).\n");
-	}else{
-		int i;
-		for(i=1;i<argc;i++){
-			FILE *fp;
-			fp = fopen(argv[i] , "r");
-			if(fp == NULL){
-				fprintf(stderr, "Error(EX_NOINPUT), uso incorrecto del mandato.\"Success\"\n");
-				fprintf(stderr, "El fichero %s no pudo ser leído\n", argv[i]);
-				return EX_NOINPUTM;
-			}
-			char str[2048];
-			char res[2048];
-			memset(str,'\0',2048);
-			memset(res,'\0',2048);
-			while(fgets(str,MAX,fp) != NULL) {
-				strtok(str, "\n");
-				int i;
-				for(i = strlen(str)-1;i>=0; i--){
-					res[(strlen(str)-1-i)]=str[i];
-				}
-				// res[strlen(str)]="\n";
-				fputs(res, stdout);
-				printf("\n");
-				memset(str,'\0',2048);
-				memset(res,'\0',2048);
-			}
-			fclose(fp);
-		}
-	}
-	return 0;
+ if(argc==1){
+char str[2048];
+char res[2048];
+
+           while(fgets(str,MAX,stdin) != NULL) {
+                                strtok(str, "\n");
+                                int i;
+                                for(i = strlen(str)-1;i>=0; i--){
+                                        res[(strlen(str)-1-i)]=str[i];
+                                }
+                                fputs(res, stdout);
+                                printf("\n");
+                                memset(str,'\0',2048);
+                                memset(res,'\0',2048);
+                        }
+            }
+
+	else if(argc==2&&(strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)){
+                        fprintf(stdout, "delreves: Uso: delreves [ fichero... ]\n");
+                        fprintf(stdout, "delreves: Invierte el contenido de las lineas de los ficheros (o de la entrada).\n");
+        }
+
+        else{
+                int i;
+                for(i=1;i<argc;i++){
+                       	FILE *fp;
+                       	fp = fopen(argv[i] , "r");
+                        if(fp == NULL){
+                               	if(errno==2){
+                                 fprintf(stderr, "delreves: Error(EX_NOINPUT), uso incorrecto del mandato.\"Success\"\n");
+                                fprintf(stderr, "delreves+.*\"%s\" no pudo ser leído\n", argv[i]);
+                                 return EX_NOINPUT;
+
+                                }
+                                else{
+                                fprintf(stderr, "delreves: Error(EX_NOINPUT), uso incorrecto del mandato.\"Success\"\n");
+                                fprintf(stderr, "delreves+.*\"%s\" no pudo ser leído\n", argv[i]);
+                                 return EX_NOINPUT;
 }
+                        }
+                        else{
+                        char str[2048];
+                        char res[2048];
+                        memset(str,'\0',2048);
+                        memset(res,'\0',2048);
+                        while(fgets(str,MAX,fp) != NULL) {
+                                strtok(str, "\n");
+                                int i;
+                                for(i = strlen(str)-1;i>=0; i--){
+                                        res[(strlen(str)-1-i)]=str[i];
+					  }
+                               	fputs(res, stdout);
+                               	printf("\n");
+                                memset(str,'\0',2048);
+                                memset(res,'\0',2048);
+                                                }
+                        fclose(fp);
+                }
+}
+        }
+        return 0;
+}
+
+
+
+
+
+
